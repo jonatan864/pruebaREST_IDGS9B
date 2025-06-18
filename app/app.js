9e32d2ecea7988c81c0f96a63cd1f4034f92d97a
Jonathan <2236000300@utna.edu.mx>
2025-06-18 17:51:29 -0600

    creacion del primer endpoint en la API

--- app/app.js
+++ app/app.js
@@ -1,4 +1,9 @@
 const express = require('express')
 const app = express()
+const router = require('./routes/gymRouter')
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+app.use('/gym', router)
 
 module.exports = app;
