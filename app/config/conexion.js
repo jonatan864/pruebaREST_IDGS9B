96699f3ccde2151ef4df25d23e51a7726f97bc7d
Jonathan <2236000300@utna.edu.mx>
2025-06-20 17:01:15 -0600

    se realiza la conexion a la base de datos

---
+++ app/config/conexion.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const config = require('./configuracion')
+
+module.exports = {
+    connection: null,
+    connect: () => {
+        if (this.connection) {
+            return this.connection
+            return mongoose.connect(config.DB)
+            .then(conn => {
+                this.connection = conn
+                console.log("La conexion se ha realizado de manera correcta")
+            })
+            .catch(e => {console.log('Error en la conexion', e)})
+        }
+    }
+}
