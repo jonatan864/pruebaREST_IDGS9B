ef2389b4ed5f89a1d87a45b24d16980e099ca5a6
Jonathan <2236000300@utna.edu.mx>
2025-07-02 17:51:23 -0600

    Se crea el controlador y se realiza el primer endpoint

--- app/config/conexion.js
+++ app/config/conexion.js
@@ -4,7 +4,7 @@ const config = require('./configuracion')
 module.exports = {
     connection: null,
     connect: () => {
-        if (this.connection) {
+        if (this.connection) 
             return this.connection
             return mongoose.connect(config.DB)
             .then(conn => {
@@ -12,6 +12,6 @@ module.exports = {
                 console.log("La conexion se ha realizado de manera correcta")
             })
             .catch(e => {console.log('Error en la conexion', e)})
-        }
+        
     }
 }
